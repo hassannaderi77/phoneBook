67a2535f3c24062d27a0f0305fc9fd6a6da138f0
hassannaderi77 <hasannaderi77@gmail.com>
2025-09-24 16:35:33 +0330

    set header

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -7,15 +7,15 @@ export default async function handler(req, res) {
 
   try {
     res.setHeader(
-      "Set-Cookie",
-      serialize("token", "", {
-        httpOnly: true, //XSS
-        path: "/",
-        maxAge: 0,
-        sameSite: "none",
-        secure: true,
-      })
-    );
+    "Set-Cookie",
+    serialize("token", "", {
+      httpOnly: true,
+      path: "/",           // حتما همین مسیر باشه
+      maxAge: 0,           // صفر یعنی حذف
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
     return res.status(200).json({ message: "User Logged Out Successfully" });
   } catch (error) {
     res.status(500).json({ message: "server error" });
